cfb5bdf5c220c6c19ec3c0304a8dab30cb105b27
Darya <minakovaa.darya@gmail.com>
2025-11-10 12:09:15 +0300

    Update solution_part1.py

--- linear_algebraic_systems/solution_part1.py
+++ linear_algebraic_systems/solution_part1.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 
 
@@ -19,34 +18,37 @@ class LinearAlgebraicSystems:
 
         for k in range(self.n):  # K IS A LINE NUMBERS
             max_index_in_subcol = np.argmax(
-                np.abs(self.U[self.p[k:], k]))  # index in minor, using p because some lines we swapped
+                np.abs(self.U[k:, k]))  # index in minor, using p because some lines we swapped
             max_index_in_col = max_index_in_subcol + k
             if k != max_index_in_col:
                 self.p[[max_index_in_col, k]] = self.p[[k, max_index_in_col]]
-                self.L[[max_index_in_col, k]] = self.L[[k, max_index_in_col]]
+                self.U[[max_index_in_col, k]] = self.U[[k, max_index_in_col]]
                 self.swaps += 1
 
-            pivot = self.U[self.p[k], k]
+            pivot = self.U[k, k]
             for i in range(k + 1, n):
-                self.L[i, k] = self.U[self.p[i], k] / pivot  # L без перестановок
-                self.U[self.p[i], k:] -= self.L[i, k] * self.U[self.p[k], k:]
+                self.U[i, k] /= pivot  # L без перестановок
+                self.U[i, k + 1:] -= self.U[i, k] * self.U[k, k + 1:]
+        self.L += np.tril(self.U) - np.diag(np.diag(self.U))
+        self.U = np.triu(self.U)
+        # print(self.L, self.U, self.p, self.L @ self.U - self.A[self.p], sep='\n')
 
 
     def determinant(self):
-        self.determinant = np.prod(np.diag(self.U[self.p])) * (-1) ** self.swaps
+        self.determinant = np.prod(np.diag(self.U)) * (-1) ** self.swaps
         return self.determinant
 
 
     def linear_system_solution(self, A=None, b=None):
         self.A = A if A is not None else self.A
         self.b = b if b is not None else self.b
-        y = np.zeros(self.n)
+        y = self.b[self.p]
         self.x = np.zeros(self.n)
 
         for i in range(self.n):
-            y[i] = self.b[self.p][i] - np.dot(self.L[i, :i], y[:i])
+            y[i] -= np.dot(self.L[i, :i], y[:i])
         for i in range(self.n - 1, -1, -1):
-            self.x[i] = (y[i] - np.dot(self.U[self.p][i, i + 1:], self.x[i + 1:])) / self.U[self.p][i, i]
+            self.x[i] = (y[i] - np.dot(self.U[i, i + 1:], self.x[i + 1:])) / self.U[i, i]
         return self.x
 
 
@@ -77,8 +79,9 @@ n = 4
 A = np.random.randint(1, 10, size=(n, n)).astype(float)
 b = np.random.randint(1, 10, size=n).astype(float)
 s = LinearAlgebraicSystems(n, A, b)
+print(f'Matrix A is')
 s.print_matrix()
-# print(f'My determinant: {s.determinant()}\nReal determinant: {np.linalg.det(A)}\n')
-# print(f'My solution: {s.linear_system_solution()}\nReal solution: {np.linalg.solve(A, b)}')
-# print(f'Inverse matrix for matrix A is \n{s.find_inverse_matrix()}')
+print(f'Determinant: {s.determinant()}')
+print(f'Solution: {s.linear_system_solution()}')
+print(f'Inverse matrix for matrix A is \n{s.find_inverse_matrix()}')
 print(f'Condition number for matrix A is {s.condition_number()}')
